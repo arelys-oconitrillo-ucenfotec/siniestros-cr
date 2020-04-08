416839d62d1dd9ca4e7666a13c34df82cfd10887
Arelys Oconitrillo <areoch_30@hotmail.com>
2020-04-07 19:39:02 -0600

    Agregando listar tipos incidentes

---
+++ public/js/admin.listar.tipo_incidentes.controlador.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const tbody = document.querySelector('#tbl-tipo-incidente tbody');
+
+let mostrar_datos = async() => {
+    let tipo_incidentes = await listar_tipo_incidentes();
+    tbody.innerHTML = '';
+
+    for (let i = 0; i < tipo_incidentes.length; i++) {
+        let fila = tbody.insertRow();
+        fila.insertCell().innerHTML = tipo_incidentes[i]['nombre_siniestro'];
+        fila.insertCell().innerHTML = tipo_incidentes[i]['icono'];
+    }
+};
+
+mostrar_datos();
--- public/js/admin.tipo_incidentes.servicio.js
+++ public/js/admin.tipo_incidentes.servicio.js
@@ -21,21 +21,3 @@ let registrar_tipo_incidente = async() => {
 
     return response;
 }
-
-let listar_tipo_incidentes = async() => {
-    let tipo_incidentes;
-
-    await axios({
-        method: 'get',
-        url: 'http://localhost:3000/api/listar/tipo-incidentes',
-        responseType: 'json'
-    }).then(function(res) {
-        console.log(res);
-        tipo_incidentes = res.data.lista_incidentes;
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
-
-    return tipo_incidentes;
-};
