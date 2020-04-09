257c7db93195e3890bc98096cee9a053be3caa31
Arelys Oconitrillo <areoch_30@hotmail.com>
2020-04-09 12:14:42 -0600

    Registrar tipo de incidente listo y agregar foto del usuario

--- public/js/admin.tipo_incidentes.servicio.js
+++ public/js/admin.tipo_incidentes.servicio.js
@@ -1,5 +1,23 @@
 'use strict';
 
+let listar_tipo_incidentes = async() => {
+    let tipo_incidentes;
+
+    await axios({
+        method: 'get',
+        url: 'http://localhost:3000/api/listar/tipo-incidentes',
+        responseType: 'json'
+    }).then(function(res) {
+        console.log(res);
+        tipo_incidentes = res.data.lista_incidentes;
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+
+    return tipo_incidentes;
+};
+
 
 let registrar_tipo_incidente = async() => {
     await axios({
