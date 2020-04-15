15a54727c5c481857e4f45b790cfcea9e0e32242
Arelys Oconitrillo <areoch_30@hotmail.com>
2020-04-14 19:25:42 -0600

    Agregando inicio sesión usuario especializado

---
+++ public/js/inicio.usuario_especializado.controlador.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const inputCorreo = document.getElementById('txtCorreo');
+const inputContrasena = document.getElementById('txtContrasena');
+const botonIngresar = document.getElementById('btnIngresar');
+
+function obtenerDatos() {
+    console.log("obtenerDatos");
+    let correo = inputCorreo.value;
+    let contrasena = inputContrasena.value;
+
+    let errorBlancos = validarUsuario(correo, contrasena);
+    let usuarioAceptado = false;
+
+    if (errorBlancos) {
+        Swal.fire({
+            'title': 'Sus datos no se pueden validar',
+            'text': 'Revisar los campos resaltados en ROJO',
+            'icon': 'warning'
+        });
+    } else {
+        let respuesta = validar_credenciales(correo,contrasena);
+        usuarioAceptado = respuesta.success;
+        if (usuarioAceptado) {
+            window.location.href = 'bienvenido-sesion.html';
+        } else {
+            Swal.fire({
+                'title': 'Sus datos no se pueden validar',
+                'text': 'Usuario o contraseña incorrectos',
+                'icon': 'warning'
+            });
+        }
+    }
+
+};
+
+let validarUsuario = () => {
+    let campos_requeridos = document.querySelectorAll('#frm-inicio-sesion [required]');
+    let error = false;
+
+    for (let i = 0; i < campos_requeridos.length; i++) {
+        if (campos_requeridos[i].value == '') {
+            campos_requeridos[i].classList.add('input-error');
+            error = true;
+        } else {
+            campos_requeridos[i].classList.remove('input-error');
+        }
+    }
+
+    if (!/^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/.test(inputCorreo.value)) {
+        inputCorreo.classList.add('input-error');
+        error = true;
+    }
+
+    return error;
+      
+};
+
+botonIngresar.addEventListener('click', obtenerDatos);
---
+++ public/js/inicio.usuario_especializado.servicio.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function validar_credenciales(pcorreo, pcontrasena) {
+    let respuesta = '';
+    let peticion = $.ajax({
+        url: 'http://localhost:3000/api/validar_credenciales',
+        type: 'post',
+        contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+        dataType: 'json',
+        //responseType: 'json',
+        async: false,
+        data: {
+            correo: pcorreo,
+            contrasena: pcontrasena
+        }
+    });
+
+    peticion.done(function (response) {
+        respuesta = response;
+        if(respuesta.success){
+            sessionStorage.setItem('conectado', response.success); //resultado viene de users.api
+            sessionStorage.setItem('tipo_usuario', response.usuario_normal.rol);
+            sessionStorage.setItem('nombre', response.usuario_normal.primer_nombre);
+            sessionStorage.setItem('nombre_comercial', response.usuario_normal.nombre_comercial);
+            sessionStorage.setItem('apellido', response.usuario_normal.primer_apellido);
+            sessionStorage.setItem('correo', response.usuario_normal.correo);
+        }
+    });
+
+    peticion.fail(function (response) {
+        respuesta = response;
+        console.log("Request fail error:" + respuesta);
+    });
+
+    return respuesta;
+};
