c209c9625518108a530a72fcced3bcbfc49cf27e
Arelys Oconitrillo <areoch_30@hotmail.com>
2020-04-19 12:50:20 -0600

    Agregando editar usuario normal

--- public/js/editar.usuario_normal.controlador.js
+++ public/js/editar.usuario_normal.controlador.js
@@ -1,15 +1,18 @@
 'use strict';
 let identificacion = localStorage.getItem('identificacion_usuario_normal');
+const botonIdentificacion = document.querySelector('#sltTipoIdentificacion');
 
-const botonRegistrar = document.querySelector('#btnRegistrar');
+
 const input_primer_nombre = document.querySelector('#txtPrimerNombre');
 const input_segundo_nombre = document.querySelector('#txtSegundoNombre');
 const input_primer_apellido = document.querySelector('#txtPrimerApellido');
 const input_segundo_apellido = document.querySelector('#txtSegundoApellido');
+const input_tipo_identificacion = document.querySelector('#sltTipoIdentificacion');
+const input_identificacion = document.querySelector('#txtIdentificacion');
 const input_correo = document.querySelector('#txtEmail');
 const input_telefono = document.querySelector('#txtTelefono');
-const input_fotografia = document.querySelector('#txtUrlImg');
 const input_img = document.querySelector('#icon-img');
+const botonRegistrar = document.querySelector('#btnRegistrar');
    
 
 let llenar_campos = async() => {
@@ -19,16 +22,17 @@ let llenar_campos = async() => {
     input_segundo_nombre.value = usuario_normal.segundo_nombre;
     input_primer_apellido.value = usuario_normal.primer_apellido;
     input_segundo_apellido.value = usuario_normal.segundo_apellido;
+    input_tipo_identificacion.value = usuario_normal.tipo_identificacion;
+    input_identificacion.value = usuario_normal.identificacion;
     input_correo.value = usuario_normal.correo;
     input_telefono.value = usuario_normal.telefono;
-    input_fotografia.value = usuario_normal.fotografia;
-    input_img.src = usuario_normal.img;
+    input_img.src = usuario_normal.fotografia;
 
-    switch(usuario_normal.field-genero){
-        case 'Femenino':
+    switch(usuario_normal.genero){
+        case 'femenino':
             document.querySelector('#rbtFemenino').checked = true;
         break;
-        case 'Masculino':
+        case 'masculino':
             document.querySelector('#rbtMasculino').checked = true;
         break;
     }
@@ -39,3 +43,116 @@ let llenar_campos = async() => {
 };
 
 llenar_campos();
+
+let validar = () => {
+    let campos_requeridos = document.querySelectorAll('#frm-registro [required]');
+    let input_genero = document.querySelector('#field-genero input[type=radio]:checked');
+    let error = false;
+
+    for (let i = 0; i < campos_requeridos.length; i++) {
+        let label_campo_requerido = document.querySelector('[for="' + campos_requeridos[i].id + '"]');
+        if (campos_requeridos[i].value == '') {
+            label_campo_requerido.classList.add('label-error');
+
+            if (campos_requeridos[i].id == 'txtInfoAponderado'){
+                campos_requeridos[i].classList.add('input-error');
+            }
+            error = true;
+        } else {
+            label_campo_requerido.classList.remove('label-error');
+            
+            if (campos_requeridos[i].id == 'txtInfoAponderado'){
+                campos_requeridos[i].classList.remove('input-error');
+            }
+        }
+    }
+
+    if (!input_genero) {
+        error = true;
+        document.querySelector('#field-genero').classList.add('input-error');
+    } else {
+        document.querySelector('#field-genero').classList.remove('input-error');
+    }
+    
+    error = validarEmail(error);
+
+    return error;
+      
+};
+
+let validarEmail = (pError) => {
+    let error = pError;
+
+    if (!error){
+        if (txtEmail.value.includes('@')){
+            document.querySelector('#txtEmail').classList.remove('input-error');
+        }else{
+            error = true;
+            document.querySelector('#txtEmail').classList.add('input-error');
+        }
+        
+    }
+    
+    return error;
+};
+
+let limpiar = () => {
+    txtPrimerNombre.value = "";
+    txtSegundoNombre.value = "";
+    txtPrimerApellido.value = "";
+    txtSegundoApellido.value = "";
+    txtEmail.value = "";
+    txtTelefono.value = "";
+    sltTipoIdentificacion.value = "";
+    txtIdentificacion.value = "";
+    txtUrlImg.value = "";
+    rbtFemenino.checked = false;
+    rbtMasculino.checked = false;
+    txtRazonSocial.value = "";
+    txtNombreComercial.value = "";
+    txtInfoAponderado.value = "";
+    document.querySelector('#icon-img').src = "../css/imgs/blank-profile-picture-973460_1280.png";
+};
+
+let establecer_identificacion = () => {
+    reiniciar_formulario_identificacion();
+
+    if(botonIdentificacion.value == ''){
+        document.querySelector('#contenedor-identificacion').classList.add('ocultar');
+        document.querySelector('#lblTipoIdentificacion').classList.remove('form__group_field_selected'); 
+    } else {
+        document.querySelector('#contenedor-identificacion').classList.remove('ocultar');
+        document.querySelector('#lblTipoIdentificacion').classList.add('form__group_field_selected'); 
+        
+        if(botonIdentificacion.value == 'jurídica'){
+            let contenedores_juridicos = document.querySelectorAll('.contenedor-juridica');
+    
+            for(let i = 0; i < contenedores_juridicos.length; i++){
+                if(contenedores_juridicos[i].querySelector('input') != null){
+                    contenedores_juridicos[i].querySelector('input').setAttribute('required', 'required');
+                } else if(contenedores_juridicos[i].querySelector('textarea') != null){
+                    contenedores_juridicos[i].querySelector('textarea').setAttribute('required', 'required');
+                }
+    
+                contenedores_juridicos[i].classList.remove('ocultar');
+            }
+        }
+    }
+};
+
+let reiniciar_formulario_identificacion = () => {
+    document.querySelector('#contenedor-identificacion').classList.add('ocultar');
+    let contenedores_juridicos = document.querySelectorAll('.contenedor-juridica');
+
+    for(let i = 0; i < contenedores_juridicos.length; i++){
+        if(contenedores_juridicos[i].querySelector('input') != null){
+            contenedores_juridicos[i].querySelector('input').removeAttribute('required');
+        } else if(contenedores_juridicos[i].querySelector('textarea')){
+            contenedores_juridicos[i].querySelector('textarea').removeAttribute('required');
+        }
+
+        contenedores_juridicos[i].classList.add('ocultar');
+    }
+};
+
+botonIdentificacion.addEventListener('input', establecer_identificacion);
