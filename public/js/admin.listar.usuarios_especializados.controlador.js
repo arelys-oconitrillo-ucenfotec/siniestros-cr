ccf677585ff3fb532df7c16d9f3e4459737aa03e
Arelys Oconitrillo <areoch_30@hotmail.com>
2020-04-21 19:43:51 -0600

    Listar usuarios especializados

---
+++ public/js/admin.listar.usuarios_especializados.controlador.js
@@ -0,0 +1,34 @@
+'use strict';
+
+obtener_menu();
+
+const tbody = document.querySelector('#tbl-listar tbody');
+
+let mostrar_datos = async() => {
+    let usuarios = await listar_usuarios_especializados();
+    tbody.innerHTML = '';
+
+    for (let i = 0; i < usuarios.length; i++) {
+        let fila = tbody.insertRow();
+
+        fila.insertCell().innerHTML = usuarios[i]['primer_nombre'];
+        fila.insertCell().innerHTML = usuarios[i]['primer_apellido'];
+        fila.insertCell().innerHTML = usuarios[i]['identificacion'];
+        fila.insertCell().innerHTML = usuarios[i]['correo'];
+    
+
+        let celda_editar = fila.insertCell();
+        let boton_editar = document.createElement('button');
+        boton_editar.type = 'button';
+        boton_editar.innerText = 'Editar'; 
+
+        boton_editar.addEventListener('click', ()=> {
+            localStorage.setItem('identificacion_usuario_especializado', usuarios[i]['identificacion']);
+            window.location.href = 'editar-usuario-especializado.html'; 
+        });
+
+        celda_editar.appendChild(boton_editar);
+    }
+};
+
+mostrar_datos();
