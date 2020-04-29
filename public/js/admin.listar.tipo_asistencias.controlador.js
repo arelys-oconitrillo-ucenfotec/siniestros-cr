1607146d04675af32adab96d53cf0cd20e98d6d1
Arelys Oconitrillo <areoch_30@hotmail.com>
2020-04-29 16:46:14 -0600

    Update admin.listar.tipo_asistencias.controlador.js

--- public/js/admin.listar.tipo_asistencias.controlador.js
+++ public/js/admin.listar.tipo_asistencias.controlador.js
@@ -14,18 +14,21 @@ let mostrar_datos = async() => {
         fila.insertCell().innerHTML = '<p>' + tipo_asistencias[i]['descripcion'] + '</p>';
         fila.insertCell().innerHTML = '<p>' + tipo_asistencias[i]['costo'] + '</p>';
         
-        if ( ){
-            let celda_editar = fila.insertCell();
-            let boton_editar = document.createElement('button');
-            boton_editar.type = 'button';
-            boton_editar.innerText = 'Editar'; 
-    
-            boton_editar.addEventListener('click', ()=> {
-                localStorage.setItem('nombre_asistencia', tipo_asistencias[i]['nombre_asistencia']);
-                window.location.href = 'editar-tipo-asistencia.html'; 
-            });
-    
-            celda_editar.appendChild(boton_editar);
+        if (localStorage.getItem('conectado')){
+            if (localStorage.getItem('tipo_usuario') == 'admin'){
+                let celda_editar = fila.insertCell();
+                let boton_editar = document.createElement('button');
+                boton_editar.type = 'button';
+                boton_editar.innerText = 'Editar'; 
+        
+                boton_editar.addEventListener('click', ()=> {
+                    localStorage.setItem('nombre_asistencia', tipo_asistencias[i]['nombre_asistencia']);
+                    window.location.href = 'editar-tipo-asistencia.html'; 
+                });
+        
+                celda_editar.appendChild(boton_editar);
+            }
+           
         } 
 
        
